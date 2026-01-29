e2b7165c09a7a4dc107c4cdf065b5cc118a6cd4d
Eduard Slobodyanik <slobodyanik.ed@gmail.com>
2026-01-29 15:54:49 +0300

    style check

--- app/infrastructure/database/models.py
+++ app/infrastructure/database/models.py
@@ -1,13 +1,4 @@
-
-from sqlalchemy import (
-    Column,
-    DateTime,
-    ForeignKey,
-    Integer,
-    Numeric,
-    String,
-    Text,
-)
+from sqlalchemy import Column, DateTime, ForeignKey, Integer, Numeric, String, Text
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 
